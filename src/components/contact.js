a6825a11aa6b5cbfade4298cd9df3ea8adee9c07
Maria Jose Tamayo <mariajose.tamayo@gmail.com>
2017-04-20 21:40:28 -0600

    corrected other links

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,7 +11,7 @@ export default function Contact (props){
     fontSize: '35px'
   }
   const profStyle = {
-    color:'#707070',
+    color:'#282828',
     fontSize: '20px',
     padding: '3%'
   }
@@ -43,7 +43,7 @@ export default function Contact (props){
       <hr />
       <h1 style= {nameStyle}>Contact</h1>
       <p style={profStyle}>I always look forward to hear about new projects and challenges! Here are a few ways to contact me: </p>
-      <p style={logosLineStyle}><a href="mailto:mariajose.tamayo@gmail.com"><i className="fa fa-envelope" aria-hidden="true" style={emailLogoStyles}></i></a><a href="https://github.com/mariajosetamayo"><i className="fa fa-github fa-lg" aria-hidden="true" style={githubLogoStyles}></i></a><a href="https://www.linkedin.com/in/maria-tamayo-web-developer/"><i className="fa fa-linkedin-square" aria-hidden="true" style={websiteLinkStyles}></i></a><a href="https://www.facebook.com/mariajose.tamayo"><i className="fa fa-facebook-square" aria-hidden="true" style={websiteLinkStyles}></i></a><a href="https://www.instagram.com/mjtmayo/"><i className="fa fa-instagram" aria-hidden="true" style={websiteLinkStyles}></i></a></p>
+      <p style={logosLineStyle}><a href="mailto:mariajose.tamayo@gmail.com"><i className="fa fa-envelope" aria-hidden="true" style={emailLogoStyles}></i></a><a href="https://github.com/mariajosetamayo" target="_blank"><i className="fa fa-github fa-lg" aria-hidden="true" style={githubLogoStyles}></i></a><a href="https://www.linkedin.com/in/maria-tamayo-web-developer/" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true" style={websiteLinkStyles}></i></a><a href="https://www.facebook.com/mariajose.tamayo" target="_blank"><i className="fa fa-facebook-square" aria-hidden="true" style={websiteLinkStyles}></i></a><a href="https://www.instagram.com/mjtmayo/" target="_blank"><i className="fa fa-instagram" aria-hidden="true" style={websiteLinkStyles}></i></a></p>
       <p><i className="fa fa-copyright" aria-hidden="true"></i>2017, Maria Jose Tamayo. All Rights Reserved</p>
 
 
