b2e0eeec36b18b4e856f8bfdabfdfb2ee8d26d48
Maria Jose Tamayo <mariajose.tamayo@gmail.com>
2017-05-09 12:57:50 -0600

    project images now link to projects

--- src/components/project.js
+++ src/components/project.js
@@ -43,7 +43,7 @@ class Project extends Component {
           <h2 className = "projectName" style={titleStyles}>Outdoor Sagas</h2>
           <p className="projectDescription" style={descriptionStyles}>Outdoor Sagas is a full-stack application that allows users to record, share, and save memories of their outdoor adventures in one place.</p>
           <p><a href="https://github.com/mariajosetamayo/outdoor-sagas-app"><i className="fa fa-github fa-lg github" aria-hidden="true" style={githubLogoStyles}></i></a><a href="https://outdoor-sagas.herokuapp.com/" target="_blank"><i className="fa fa-link laptop" aria-hidden="true" style={websiteLinkStyles}></i></a></p>
-          <img src={'../photos/outdoorSagasNew.jpg'} style={sectionStyles}/>
+          <a href="https://outdoor-sagas.herokuapp.com/" target="_blank"><img src={'../photos/outdoorSagasNew.jpg'} style={sectionStyles}/></a>
         </div>
       )
     }
@@ -53,7 +53,7 @@ class Project extends Component {
           <h2 className = "projectName" style={titleStyles}>FoodTrack</h2>
           <p className="projectDescription" style={descriptionStyles}>FoodTrack is a full-stack application to keep track of what you eat, get recommendations, and a daily report summarizing the nutrients you consumed.</p>
           <p><a href="https://github.com/mariajosetamayo/foodTrack"><i className="fa fa-github fa-lg github" aria-hidden="true" style={githubLogoStyles}></i></a><a href="https://food-track.herokuapp.com/" target="_blank"><i className="fa fa-link laptop" aria-hidden="true" style={websiteLinkStyles}></i></a></p>
-          <img src={'../photos/foodTrackScreenShot2.jpg'} style={sectionStyles}/>
+          <a href="https://food-track.herokuapp.com/" target="_blank"><img src={'../photos/foodTrackScreenShot2.jpg'} style={sectionStyles}/></a>
         </div>
       )
     }
@@ -63,7 +63,7 @@ class Project extends Component {
           <h2 className = "projectName" style={titleStyles}>Muncheck</h2>
           <p className="projectDescription" style={descriptionStyles}>Muncheck is an application to check the  nutritional facts about almost any food item and whether it is recommended given a particular dietary requirement (diabetes, kidney disease and weight loss).</p>
           <p><a href="https://github.com/mariajosetamayo/muncheck"><i className="fa fa-github fa-lg github" aria-hidden="true" style={githubLogoStyles}></i></a><a href="https://mariajosetamayo.github.io/muncheck/" target="_blank"><i className="fa fa-link laptop" aria-hidden="true" style={websiteLinkStyles}></i></a></p>
-          <img src={'../photos/muncheckNew.png'} style={sectionStyles}/>
+          <a href="https://mariajosetamayo.github.io/muncheck/" target="_blank"><img src={'../photos/muncheckNew.png'} style={sectionStyles}/></a>
         </div>
       )
     }
